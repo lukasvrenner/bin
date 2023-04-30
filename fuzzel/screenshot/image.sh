4977421044277be984752820940d4fc66e93775d
lukasrenner2 <118417051+lukasrenner2@users.noreply.github.com>
2023-04-29 22:14:05 -0800

    changed file path and stuff

--- fuzzel/screenshot/image.sh
+++ fuzzel/screenshot/image.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
-
+date=`date +%d-%m-%Y_%H:%M:%S`
+file_name=~/Pictures/screenshots/capture@"$date".jpeg
 chosen=$(printf "Area\nWindow\nScreen" | fuzzel -d)
 
 case "$chosen" in
-	"Area") grim -g "$(slurp -d)" - | swappy -f - ;;
-	"Window") grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)" - | swappy -f - ;;
-    "Screen") grim - | swappy -f - ;;
+	"Area") grim -l 0 -g "$(slurp -d)" -o $file_name ;;
+	"Window") grim -l 0 -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)" -o $file_name ;;
+    "Screen") grim ;;
 	*) exit 1 ;;
 esac
