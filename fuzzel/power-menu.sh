13dd599aa33afd9483a8f18d0635d8da3cd1c72c
lukasvrenner <lukasvrenner@icloud.com>
2023-08-22 22:02:27 -0800

    fixed power-menu

--- fuzzel/power-menu.sh
+++ fuzzel/power-menu.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-chosen=$(printf "⏻  Power Off\n⟲  Restart\n  Suspend\n  Hibernate\n   Log Out\n  Lock" | fuzzel -d)
+chosen=$(printf "Power Off\nRestart\nSuspend\nHibernate\nLog Out\nLock" | fuzzel -d)
 
 case "$chosen" in
-	"⏻  Power Off") poweroff ;;
-	"⟲  Restart") reboot ;;
-	"  Suspend") systemctl suspend-then-hibernate ;;
-	"  Hibernate") systemctl hibernate ;;
-	"   Log Out") swaymsg exit ;;
-	"  Lock") ~/bin/swaylock/swaylock.sh ;;
+	"Power Off") poweroff ;;
+	"Restart") reboot ;;
+	"Suspend") systemctl suspend ;;
+	"Hibernate") systemctl hibernate ;;
+	"Log Out") swaymsg exit ;;
+	"Lock") swaylock ;;
 	*) exit 1 ;;
 esac
